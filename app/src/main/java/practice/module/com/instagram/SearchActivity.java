ef5d391ea1ed7dc5435cb481e2bd4c88b456a93e
Anshul1507 <ag1507anshul@gmail.com>
2019-03-09 11:49:05 +0530

    Commit
    Adding Bottom Naviagtion Bar, top and bottom overlay

---
+++ app/src/main/java/practice/module/com/instagram/SearchActivity.java
@@ -0,0 +1,14 @@
+package practice.module.com.instagram;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
